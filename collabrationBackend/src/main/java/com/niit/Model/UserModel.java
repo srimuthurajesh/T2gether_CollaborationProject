85df374571a6a9118d44073d9bdc1b8e9f8b07b8
srimuthurajesh <srimuthurajesh@gmail.com>
2017-02-16 07:47:50 +0530

    First commit, usermodel

---
+++ collabrationBackend/src/main/java/com/niit/Model/UserModel.java
@@ -0,0 +1,45 @@
+package com.niit.Model;
+
+public class UserModel {
+
+	private String username;
+	private String firstname;
+	private String  secondname;
+	private String email;
+	private String mobile;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getSecondname() {
+		return secondname;
+	}
+	public void setSecondname(String secondname) {
+		this.secondname = secondname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	
+	
+	
+	
+}
