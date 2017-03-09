dd5edac2a12bb6f1b142c66c36d5ab5cccbc34dd
srimuthurajesh <srimuthurajesh@gmail.com>
2017-03-09 06:58:31 +0530

    angular not wotking

--- collabrationBackend/src/main/java/com/niit/config/AppplicationContextConfig.java
+++ collabrationBackend/src/main/java/com/niit/config/AppplicationContextConfig.java
@@ -41,7 +41,7 @@ public class AppplicationContextConfig {
 	    Properties properties = new Properties();
 	    properties.put("hibernate.show_sql", "true");
 	    properties.put("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
-	   properties.put("hibernate.hbm2ddl.auto", "update");
+	  // properties.put("hibernate.hbm2ddl.auto", "update");
 	    return properties;
 	}
 	@Autowired
